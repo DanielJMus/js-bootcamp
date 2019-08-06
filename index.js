436fcec31eb280f4c79055993d2d2738c147f47b
DanielJMus <danielmus1999@hotmail.com>
2019-08-07 10:17:59 +1200

    Variable declarations

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello world");
+let name = "Nelson";
+name = "NZ";
+name = "Dan";
+console.log(name);
+
+const lastName = "JMus";
+const firstName = "Daniel";
+console.log(lastName);
+console.log(firstName);
